786a09ad01d01d6a63458a31c4cc23d57f38dfed
danyadev <nemov.danil@mail.ru>
2018-04-19 16:36:20 +0300

    Фикс автообновления

--- renderer/update.js
+++ renderer/update.js
@@ -18,20 +18,20 @@ var getGithubFiles = (user, repo, removeList, callback) => {
         let data = JSON.parse(body), dir = 0;
         
         for(let i = 0; i<Object.keys(data).length; i++) {
-          let key = Object.keys(data)[i];
+          let key = Object.keys(data)[i], _path = `.${path}/`;
           
           if(data[key].type == 'dir') {
             sendReq(path + '/' + data[key].name);
             dir = 1;
           } else {
-            if(!path) fileList.push(data[key].name);
-            else fileList.push(path + '/' + data[key].name);
+            fileList.push(_path + data[key].name);
             
             if(!dir && Object.keys(data).length-1 == i) {
               allFiles = allFiles.concat(fileList);
               
               removeList.forEach((file, i) => {
                 let index = fileList.indexOf(file);
+                
                 if(index != -1) fileList.splice(index, 1);
                 if(i == removeList.length-1) callback(fileList, allFiles);
               });
@@ -45,35 +45,31 @@ var getGithubFiles = (user, repo, removeList, callback) => {
   sendReq('');
 }
 
-var getLocalFiles = (path, callback) => {
+var getLocalFiles = callback => {
   let allFilesList = [], filesList = [];
   
-  let readDir = folderPath => {
-    let dir = 0, slash = folderPath ? '' : './';
+  let readDir = path => {
+    let dir = 0, slash = path ? '' : './';
     
-    fs.readdir(slash + folderPath, (e, files) => {
+    fs.readdir(slash + path, (e, files) => {
       for(let i = 0; i < files.length; i++) {
         if(files[i] == '.git' || files[i] == 'core') continue;
         
-        let file = folderPath ? folderPath + '/' + files[i] : files[i];
+        let file = path ? path + '/' + files[i] : files[i];
         
         if(fs.statSync(slash + file).isDirectory()) {
           readDir(slash + file);
           dir = 1;
         } else {
-          if(file.match(/^\.\//)) file = file.replace(/^\./, '');
-          
-          allFilesList.push(file);
+          allFilesList.push(slash + file);
         
-          if(!dir && files.length-1 == i) {
-            callback(allFilesList);
-          }
+          if(!dir && files.length-1 == i) callback(allFilesList);
         }
       }
     });
   }
   
-  readDir(path);
+  readDir('');
 }
 
 var update = () => {
@@ -86,43 +82,41 @@ var update = () => {
           v1 = JSON.parse(fs.readFileSync('./package.json')).version.split('.');
 
       if(!develop && (v0[0] > v1[0] || v0[1] > v1[1] || v0[2] > v1[2])) {
-        let noUpdate = ['/renderer/settings.json', '/renderer/users.json'];
+        let noUpdate = ['./renderer/settings.json', './renderer/users.json'];
         
         getGithubFiles('danyadev', 'vk-desktop-app', noUpdate, (files, allFiles) => {
-          files.forEach((file, i) => {
+          getLocalFiles(localFiles => {
+            let deleteFiles = localFiles.filter(file => !allFiles.includes(file));
+            
+            deleteFiles.forEach(file => fs.unlink(file, ()=>{}));
+          });
+          
+          files.forEach((filename, i) => {
+            let file = fs.createWriteStream(filename);
+            
             https.get({
               host: 'raw.githubusercontent.com',
-              path: `/danyadev/vk-desktop-app/master/${file}`
+              path: `/danyadev/vk-desktop-app/master/${filename}`
             }, res => {
-              let body = Buffer.alloc(0);
-          
-              res.on('data', ch => body = Buffer.concat([body, ch]));
-              res.on('end', () => {
-                fs.writeFileSync(file, body.toString());
-                
-                if(i == files.length - 1) {
-                  dialog.showMessageBox({
-                    type: 'info',
-                    buttons: ['ОК', 'Отмена'],
-                    title: 'Доступно обновление',
-                    message: 'Доступно новое обновление.',
-                    detail: 'Для обновления необходимо перезагрузить приложение.\nПродолжить?',
-                    noLink: true
-                  }, btn => {
-                    if(!btn) {
-                      app.relaunch();
-                      app.exit();
-                    }
-                  });
-                }
-              });
+              res.pipe(file);
+              // если делать полоску - то тут можно делать loaded++ и прибавлять проценты
+              
+              if(i == files.length - 1) {
+                dialog.showMessageBox({
+                  type: 'info',
+                  buttons: ['ОК', 'Отмена'],
+                  title: 'Доступно обновление',
+                  message: 'Доступно новое обновление.',
+                  detail: 'Для обновления необходимо перезагрузить приложение.\nПродолжить?',
+                  noLink: true
+                }, btn => {
+                  if(!btn) {
+                    app.relaunch();
+                    app.exit();
+                  }
+                });
+              }
             });
-          });
-
-          getLocalFiles('', localFiles => {
-            let deleteFiles = localFiles.filter(file => !allFiles.includes(file));
-
-            deleteFiles.forEach(file => fs.unlink('.' + file, ()=>{}));
           });
         });
       }
